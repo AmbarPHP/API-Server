dbdab66db05ea821f0043537146547d5010376a3
Ambar <perlamarina.franco@gmail.com>
2019-06-26 11:48:05 -0600

    hotfix

--- src/models/presupuestos.js
+++ src/models/presupuestos.js
@@ -6,6 +6,9 @@ module.exports = (sequelize, DataType) => {
       primaryKey: true,
       autoIncrement: true
     },
+    dentistaId: {
+      type: DataType.INTEGER,
+    },
     status: {
       type: DataType.BOOLEAN,
       allowNull: false,
